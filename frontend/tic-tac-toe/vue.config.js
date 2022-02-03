3e5969219c343683dc683e37606978bc7743d07d
antsa <antsa.prog@gmail.com>
2022-02-03 12:47:10 +0200

    update bootstrap 5.1.3, bootstap icons 1.7.0

--- frontend/tic-tac-toe/vue.config.js
+++ frontend/tic-tac-toe/vue.config.js
@@ -6,9 +6,9 @@ module.exports = {
             .plugin('html')
             .tap(args => {
                 args[0].title = "TicTacToe";
-                args[0].bootstrapmincss =  process.env.NODE_ENV === 'production' ? '/portal/bootstrap/bootstrap.min.css':'https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css';
-                args[0].bootstrapbundleminjs =  process.env.NODE_ENV === 'production' ? '/portal/bootstrap/bootstrap.bundle.min.js':'https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.bundle.min.js';
-                args[0].bootstrapicons =  process.env.NODE_ENV === 'production' ? '/portal/bootstrap/bootstrap-icons.css':'https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css';
+                args[0].bootstrapmincss =  process.env.NODE_ENV === 'production' ? '/portal/bootstrap/bootstrap.min.css':'https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css';
+                args[0].bootstrapbundleminjs =  process.env.NODE_ENV === 'production' ? '/portal/bootstrap/bootstrap.bundle.min.js':'https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js';
+                args[0].bootstrapicons =  process.env.NODE_ENV === 'production' ? '/portal/bootstrap/bootstrap-icons.css':'https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.0/font/bootstrap-icons.css';
                 
                 return args;
             })
