fe0b3387a0bc24116fbbfbec86820fb20fa21911
antsa <antsa.prog@gmail.com>
2022-02-22 17:59:49 +0200

    rollback tooltips using only title attribute
    full page reload caused tooltips to "vanish" as the data does not exist immediately. Additionally "User" dropdown did not work with this combination.

--- frontend/two-dimensional/src/components/Lobby.vue
+++ frontend/two-dimensional/src/components/Lobby.vue
@@ -12,10 +12,10 @@
   							<path fill-rule="evenodd" d="M15 8a.5.5 0 0 0-.5-.5H2.707l3.147-3.146a.5.5 0 1 0-.708-.708l-4 4a.5.5 0 0 0 0 .708l4 4a.5.5 0 0 0 .708-.708L2.707 8.5H14.5A.5.5 0 0 0 15 8z"/>
 						</svg>					
 					</div>					
-					<i v-if="user.rankingConnectFour>0" class="bi bi-star float-end tooltipselector" data-bs-toggle="tooltip" data-bs-placement="top" title="ConnectFour ranking">
+					<i v-if="user.rankingConnectFour>0" class="bi bi-star float-end tooltipselector"  title="ConnectFour ranking">
 						&nbsp;{{user.rankingConnectFour}}&nbsp;
 					</i>
-					<i v-if="user.rankingTictactoe>0" class="bi bi-suit-diamond float-end tooltipselector" data-bs-toggle="tooltip" data-bs-placement="top" title="TicTacToe ranking">
+					<i v-if="user.rankingTictactoe>0" class="bi bi-suit-diamond float-end tooltipselector" title="TicTacToe ranking">
 						&nbsp;{{user.rankingTictactoe}}&nbsp;
 					</i>
 				</li>
@@ -203,13 +203,6 @@ export default defineComponent({
 		}else{
 			
 		}
-	},
-	mounted(){
-		// Partly from https://therichpost.com/vue-3-bootstrap-5-tooltip-working-example/
-		const tooltips=Array.from(document.querySelectorAll('i[data-bs-toggle="tooltip"]'))
- 		tooltips.forEach(tooltipNode =>{			 
-			  new Tooltip(tooltipNode)
-		 })
 	},
 	methods: {
 		connect(token: string): void {		
