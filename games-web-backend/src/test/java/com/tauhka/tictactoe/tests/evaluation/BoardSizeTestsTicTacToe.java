794621f8696cdea4d39b41ca0a6ee587c9d69895
antsa <antsa.prog@gmail.com>
2022-03-29 18:13:54 +0200

    change test class name in order to work with maven tests

---
+++ games-web-backend/src/test/java/com/tauhka/tictactoe/tests/evaluation/BoardSizeTestsTicTacToe.java
@@ -0,0 +1,46 @@
+package com.tauhka.tictactoe.tests.evaluation;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+import com.tauhka.games.core.GameMode;
+import com.tauhka.games.core.Table;
+import com.tauhka.games.core.User;
+
+public class BoardSizeTestsTicTacToe {
+	@Test
+	public void negativeBoardSizeThrowsException() {
+		User playerA = new User("test");
+		Table c = new Table(playerA, GameMode.getGameMode(1), false);
+		User playerB = new User("best");
+		c.setPlayerB(playerB);
+		c.addGameToken(playerA, 0, 0);
+
+		IllegalArgumentException illegalArgEx = assertThrows(IllegalArgumentException.class, () -> {
+			c.addGameToken(playerB, 0, -15);
+		});
+		String expectedMessage = "Board length fail, x:0 y:-15";
+		String actualMessage = illegalArgEx.getMessage();
+
+		assertTrue(actualMessage.contains(expectedMessage));
+	}
+
+	@Test
+	public void tooBigRowThrowExpection() {
+		User playerA = new User("test");
+		Table c = new Table(playerA, GameMode.getGameMode(1), false);
+		User playerB = new User("best");
+		c.setPlayerB(playerB);
+		c.addGameToken(playerA, 0, 0);
+
+		IllegalArgumentException illegalArgEx = assertThrows(IllegalArgumentException.class, () -> {
+			c.addGameToken(playerB, 0, 115);
+		});
+		String expectedMessage = "Board length fail, x:0 y:115";
+		String actualMessage = illegalArgEx.getMessage();
+
+		assertTrue(actualMessage.contains(expectedMessage));
+	}
+}
