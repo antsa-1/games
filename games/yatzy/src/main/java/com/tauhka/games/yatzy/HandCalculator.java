ca0d466049b83d799ea109ed9520c13ae15c91ac
antsa <antsa.prog@gmail.com>
2022-06-17 21:15:22 +0300

    add more junit tests

--- games/yatzy/src/main/java/com/tauhka/games/yatzy/HandCalculator.java
+++ games/yatzy/src/main/java/com/tauhka/games/yatzy/HandCalculator.java
@@ -1,5 +1,6 @@
 package com.tauhka.games.yatzy;
 
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashSet;
 import java.util.Iterator;
@@ -16,6 +17,7 @@ import java.util.stream.Stream;
 
 public class HandCalculator {
 	private static final int NO_POINTS = 0;
+	private static final int YATZY = 50;
 
 	public static int calculateHandValue(Hand hand) {
 		int total = 0;
@@ -31,14 +33,14 @@ public class HandCalculator {
 		case FIVES:
 			return findNumberCount(5, hand) * 5;
 		case SIXES:
-			return findNumberCount(6, hand) * 5;
+			return findNumberCount(6, hand) * 6;
 		case PAIR:
-			Set<Integer> pair = searchAllPairs(hand);
+			Set<Integer> pair = searchAllPairsFromBiggestToSmallest(hand);
 			if (pair.size() == 0)
 				return NO_POINTS;
 			return pair.iterator().next() * 2;
 		case TWO_PAIR:
-			Set<Integer> pairs = searchAllPairs(hand);
+			Set<Integer> pairs = searchAllPairsFromBiggestToSmallest(hand);
 			if (pairs.size() != 2)
 				return NO_POINTS;
 			Iterator<Integer> iterator = pairs.iterator();
@@ -47,22 +49,76 @@ public class HandCalculator {
 			}
 			return total;
 		case TRIPS:
-			Set<Integer> trips = searchTrips(hand);
-			if (trips.size() != 1) {
-				return NO_POINTS;
-			}
-			total = trips.iterator().next() * 3;
-			return total;
+			int trips = getNumberWhichExistAtLeastTimes(3, hand);
+			return trips > 0 ? trips * 3 : NO_POINTS;
+		case QUADS:
+			int quads = getNumberWhichExistAtLeastTimes(4, hand);
+			return quads > 0 ? quads * 4 : NO_POINTS;
+		case YATZY:
+			int yatzy = getNumberWhichExistAtLeastTimes(5, hand);
+			return yatzy > 0 ? YATZY : NO_POINTS;
+		case SMALL_STRAIGHT:
+			boolean smallStraight = isStraight(SMALL_STRAIGHT, hand);
+			return smallStraight ? 15 : NO_POINTS;
+		case LARGE_STRAIGHT:
+			boolean large = isStraight(LARGE_STRAIGHT, hand);
+			return large ? 20 : NO_POINTS;
+		case FULL_HOUSE:
+			return findFullHouse(hand);
+		case CHANCE:
+			return addAllNumbers(hand);
 		default:
-			throw new IllegalArgumentException("No such in calculations:" + hand.getHandType());
+			throw new IllegalArgumentException("No such hand in calculations:" + hand.getHandType());
 		}
 	}
 
-	private static Set<Integer> searchTrips(Hand hand) {
-		return null;
+	private static int addAllNumbers(Hand hand) {
+		return hand.getDices().stream().map(Dice::getNumber).collect(Collectors.summingInt(Integer::intValue));
 	}
 
-	private static Set<Integer> searchAllPairs(Hand hand) {
+	private static int findFullHouse(Hand hand) {
+		List<Integer> numbers = hand.getDices().stream().map(Dice::getNumber).collect(Collectors.toList());
+		Set<Integer> ints = new HashSet<Integer>(numbers);
+		if (ints.size() != 2) {
+			return NO_POINTS;
+		}
+		Iterator<Integer> iterator = ints.iterator();
+		int firstNumber = iterator.next();
+		int secondNumber = iterator.next();
+		int freqFirst = Collections.frequency(numbers, firstNumber);
+		int freqSecond = Collections.frequency(numbers, secondNumber);
+		if (freqFirst == 2 && freqSecond == 3) {
+			return firstNumber * freqFirst + secondNumber * freqSecond;
+		}
+		if (freqFirst == 3 && freqSecond == 2) {
+			return firstNumber * freqFirst + secondNumber * freqSecond;
+		}
+
+		return NO_POINTS;
+	}
+
+	private static final List<Integer> SMALL_STRAIGHT = Arrays.asList(1, 2, 3, 4, 5);
+	private static final List<Integer> LARGE_STRAIGHT = Arrays.asList(6, 2, 3, 4, 5);
+
+	private static boolean isStraight(List<Integer> straight, Hand hand) {
+		List<Integer> numbers = hand.getDices().stream().map(Dice::getNumber).collect(Collectors.toList());
+		return numbers.containsAll(straight);
+	}
+
+	// Usage -> callees have minimumTimes of 3 -> using 5 dices, only one number can exist which has >= frequency
+	private static Integer getNumberWhichExistAtLeastTimes(int minimumTimes, Hand hand) {
+		List<Integer> numbers = hand.getDices().stream().map(Dice::getNumber).collect(Collectors.toList());
+		Set<Integer> ints = new HashSet<Integer>(numbers);
+		for (Integer i : ints) {
+			int freq = Collections.frequency(numbers, i);
+			if (freq >= minimumTimes) {
+				return i;
+			}
+		}
+		return -1;
+	}
+
+	private static Set<Integer> searchAllPairsFromBiggestToSmallest(Hand hand) {
 		List<Integer> numbers = hand.getDices().stream().map(Dice::getNumber).collect(Collectors.toList());
 		Set<Integer> pairs = new HashSet<Integer>();
 		SortedSet<Integer> sortedSet = new TreeSet<Integer>(Collections.reverseOrder());
--- games/yatzy/src/test/java/com/tauhka/games/yatzy/tests/HandCalculatorTests.java
+++ games/yatzy/src/test/java/com/tauhka/games/yatzy/tests/HandCalculatorTests.java
@@ -19,6 +19,84 @@ import com.tauhka.games.yatzy.HandType;
 public class HandCalculatorTests {
 
 	@Test
+	public void onesAreFound() {
+		Hand h = createDices(HandType.ONES, 5, 4, 2, 1, 1);
+		assertEquals(2, HandCalculator.calculateHandValue(h));
+	}
+
+	@Test
+	public void onesAreNotFound() {
+		Hand h = createDices(HandType.ONES, 5, 4, 2, 6, 4);
+		assertEquals(0, HandCalculator.calculateHandValue(h));
+	}
+
+	@Test
+	public void twossAreFound() {
+		Hand h = createDices(HandType.TWOS, 2, 4, 2, 2, 2);
+		assertEquals(8, HandCalculator.calculateHandValue(h));
+	}
+
+	@Test
+	public void twossAreNotFound() {
+		Hand h = createDices(HandType.TWOS, 5, 4, 6, 6, 4);
+		assertEquals(0, HandCalculator.calculateHandValue(h));
+	}
+
+	@Test
+	public void threesAreFound() {
+		Hand h = createDices(HandType.THREES, 3, 3, 3, 3, 3);
+		assertEquals(15, HandCalculator.calculateHandValue(h));
+	}
+
+	@Test
+	public void threesAreNotFound() {
+		Hand h = createDices(HandType.THREES, 5, 4, 6, 6, 4);
+		assertEquals(0, HandCalculator.calculateHandValue(h));
+	}
+
+	@Test
+	public void foursAreFound() {
+		Hand h = createDices(HandType.FOURS, 5, 3, 3, 3, 4);
+		assertEquals(4, HandCalculator.calculateHandValue(h));
+	}
+
+	@Test
+	public void foursAreNotFound() {
+		Hand h = createDices(HandType.FOURS, 5, 2, 6, 6, 1);
+		assertEquals(0, HandCalculator.calculateHandValue(h));
+	}
+
+	@Test
+	public void fivesAreFound() {
+		Hand h = createDices(HandType.FIVES, 5, 3, 3, 5, 4);
+		assertEquals(10, HandCalculator.calculateHandValue(h));
+	}
+
+	@Test
+	public void fivesAreNotFound() {
+		Hand h = createDices(HandType.FIVES, 1, 2, 6, 6, 1);
+		assertEquals(0, HandCalculator.calculateHandValue(h));
+	}
+
+	@Test
+	public void sixesAreFound() {
+		Hand h = createDices(HandType.SIXES, 6, 3, 6, 5, 6);
+		assertEquals(18, HandCalculator.calculateHandValue(h));
+	}
+
+	@Test
+	public void sixesAreNotFound() {
+		Hand h = createDices(HandType.SIXES, 1, 2, 3, 3, 1);
+		assertEquals(0, HandCalculator.calculateHandValue(h));
+	}
+
+	@Test
+	public void noPairIsFound() {
+		Hand h = createDices(HandType.PAIR, 1, 2, 3, 4, 5);
+		assertEquals(0, HandCalculator.calculateHandValue(h));
+	}
+
+	@Test
 	public void pairIsFoundInTheBeginningOfList() {
 		Hand h = createDices(HandType.PAIR, 1, 1, 3, 4, 5);
 		assertEquals(2, HandCalculator.calculateHandValue(h));
@@ -35,35 +113,173 @@ public class HandCalculatorTests {
 		Hand h = createDices(HandType.PAIR, 2, 1, 3, 6, 6);
 		assertEquals(12, HandCalculator.calculateHandValue(h));
 	}
-	
+
 	@Test
 	public void pairIsFoundWhenAllNumbersAreSame() {
 		Hand h = createDices(HandType.PAIR, 4, 4, 4, 4, 4);
 		assertEquals(8, HandCalculator.calculateHandValue(h));
 	}
 
-
 	@Test
 	public void highestPairIsFound() {
 		Hand h = createDices(HandType.PAIR, 2, 2, 3, 5, 5);
 		assertEquals(10, HandCalculator.calculateHandValue(h));
 	}
+
 	@Test
 	public void highestPairIsFound2() {
 		Hand h = createDices(HandType.PAIR, 2, 4, 6, 4, 6);
 		assertEquals(12, HandCalculator.calculateHandValue(h));
 	}
-	
+
 	@Test
 	public void twoPairIsFound() {
 		Hand h = createDices(HandType.TWO_PAIR, 2, 2, 3, 5, 5);
 		assertEquals(14, HandCalculator.calculateHandValue(h));
 	}
-	
+
 	@Test
 	public void twoPairIsFoundFromFullHouse() {
 		Hand h = createDices(HandType.TWO_PAIR, 4, 5, 4, 5, 5);
 		assertEquals(18, HandCalculator.calculateHandValue(h));
+	}
+
+	@Test
+	public void noTwoPairIsFound() {
+		Hand h = createDices(HandType.TWO_PAIR, 4, 5, 4, 4, 4);
+		assertEquals(0, HandCalculator.calculateHandValue(h));
+	}
+
+	@Test
+	public void noTwoPairIsFound2() {
+		Hand h = createDices(HandType.TWO_PAIR, 1, 2, 3, 4, 5);
+		assertEquals(0, HandCalculator.calculateHandValue(h));
+	}
+
+	@Test
+	public void tripsIsFoundFromBeginning() {
+		Hand h = createDices(HandType.TRIPS, 1, 1, 1, 4, 5);
+		assertEquals(3, HandCalculator.calculateHandValue(h));
+	}
+
+	@Test
+	public void tripsFromMiddlePositions() {
+		Hand h = createDices(HandType.TRIPS, 1, 2, 2, 2, 4);
+		assertEquals(6, HandCalculator.calculateHandValue(h));
+	}
+
+	@Test
+	public void tripsFromFiveOfAKindFound() {
+		Hand h = createDices(HandType.TRIPS, 5, 5, 5, 5, 5);
+		assertEquals(15, HandCalculator.calculateHandValue(h));
+	}
+
+	@Test
+	public void tripsFromFourOfAKindFound() {
+		Hand h = createDices(HandType.TRIPS, 2, 4, 2, 5, 2);
+		assertEquals(6, HandCalculator.calculateHandValue(h));
+	}
+
+	@Test
+	public void tripsFromMiddlePositionFound() {
+		Hand h = createDices(HandType.TRIPS, 2, 3, 3, 3, 2);
+		assertEquals(9, HandCalculator.calculateHandValue(h));
+	}
+
+	@Test
+	public void tripsNotFound() {
+		Hand h = createDices(HandType.TRIPS, 6, 5, 4, 3, 2);
+		assertEquals(0, HandCalculator.calculateHandValue(h));
+	}
+
+	@Test
+	public void quadsFromEndPositionFound() {
+		Hand h = createDices(HandType.QUADS, 2, 1, 1, 1, 1);
+		assertEquals(4, HandCalculator.calculateHandValue(h));
+	}
+
+	@Test
+	public void quadsNotFound() {
+		Hand h = createDices(HandType.QUADS, 5, 5, 5, 6, 6);
+		assertEquals(0, HandCalculator.calculateHandValue(h));
+	}
+
+	@Test
+	public void yatzyNotFound() {
+		Hand h = createDices(HandType.YATZY, 5, 5, 5, 5, 6);
+		assertEquals(0, HandCalculator.calculateHandValue(h));
+	}
+
+	@Test
+	public void yatzyFound() {
+		Hand h = createDices(HandType.YATZY, 2, 2, 2, 2, 2);
+		assertEquals(50, HandCalculator.calculateHandValue(h));
+	}
+
+	@Test
+	public void smallStraightFound() {
+		Hand h = createDices(HandType.SMALL_STRAIGHT, 5, 4, 1, 3, 2);
+		assertEquals(15, HandCalculator.calculateHandValue(h));
+	}
+
+	@Test
+	public void smallStraightNotFound() {
+		Hand h = createDices(HandType.SMALL_STRAIGHT, 6, 5, 4, 3, 2);
+		assertEquals(0, HandCalculator.calculateHandValue(h));
+	}
+
+	@Test
+	public void smallStraightNotFound2() {
+		Hand h = createDices(HandType.SMALL_STRAIGHT, 1, 1, 2, 3, 4);
+		assertEquals(0, HandCalculator.calculateHandValue(h));
+	}
+
+	@Test
+	public void largeStraightFound() {
+		Hand h = createDices(HandType.LARGE_STRAIGHT, 4, 2, 3, 6, 5);
+		assertEquals(20, HandCalculator.calculateHandValue(h));
+	}
+
+	@Test
+	public void largeStraightNotFound() {
+		Hand h = createDices(HandType.LARGE_STRAIGHT, 1, 2, 3, 4, 5);
+		assertEquals(0, HandCalculator.calculateHandValue(h));
+	}
+
+	@Test
+	public void fullHouseFound() {
+		Hand h = createDices(HandType.FULL_HOUSE, 6, 5, 5, 6, 5);
+		assertEquals(27, HandCalculator.calculateHandValue(h));
+	}
+
+	@Test
+	public void fullHouseFound2() {
+		Hand h = createDices(HandType.FULL_HOUSE, 2, 2, 2, 3, 3);
+		assertEquals(12, HandCalculator.calculateHandValue(h));
+	}
+
+	@Test
+	public void fullHouseNotFound() {
+		Hand h = createDices(HandType.FULL_HOUSE, 2, 2, 2, 2, 3);
+		assertEquals(0, HandCalculator.calculateHandValue(h));
+	}
+
+	@Test
+	public void fullHouseNotFound2() {
+		Hand h = createDices(HandType.FULL_HOUSE, 6, 1, 5, 5, 5);
+		assertEquals(0, HandCalculator.calculateHandValue(h));
+	}
+
+	@Test
+	public void chanceCalculation() {
+		Hand h = createDices(HandType.CHANCE, 6, 1, 5, 5, 5);
+		assertEquals(22, HandCalculator.calculateHandValue(h));
+	}
+
+	@Test
+	public void chanceCalculation2() {
+		Hand h = createDices(HandType.CHANCE, 1, 1, 2, 2, 2);
+		assertEquals(8, HandCalculator.calculateHandValue(h));
 	}
 
 	private Hand createDices(HandType type, int first, int second, int third, int fourth, int fifth) {
