9d1c6311b4bf17f069852d6abec1a1490339bc87
antsa <antsa.prog@gmail.com>
2022-07-13 13:33:56 +0300

    allow delay before computer starts playing it's moves

--- core-backend/src/main/java/com/tauhka/games/core/events/AITurnEvent.java
+++ core-backend/src/main/java/com/tauhka/games/core/events/AITurnEvent.java
@@ -9,14 +9,20 @@ import com.tauhka.games.core.tables.Table;
 public class AITurnEvent {
 
 	private Table table;
+	private int delay;
 
-	public AITurnEvent(Table table) {
+	public AITurnEvent(Table table, int delay) {
 		super();
 		this.table = table;
+		this.delay = delay;
 	}
 
 	public Table getTable() {
 		return table;
+	}
+
+	public int getDelay() {
+		return delay;
 	}
 
 	public void setTable(Table table) {
